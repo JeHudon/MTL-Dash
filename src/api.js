7ada9663d1aedbc1e4b28e4606b0209377e951e9
Jérôme Hudon <hudonj18@gmail.com>
2026-03-17 15:15:50 -0400

    Try fix

--- src/api.js
+++ src/api.js
@@ -1,44 +1,42 @@
-const BASE_URL = "/api";
-
 // Récuperer liste des joueurs et infos sur eux selon saison
 export async function getRoster(season) {
-    const response = await fetch(`${BASE_URL}/roster/MTL/${season}`);
+    const response = await fetch(`/api/roster/MTL/${season}`);
     return response.json();
 }
 
 // Récuperer calendrier par mois
 export async function getSchedule() {
-    const response = await fetch(`${BASE_URL}/club-schedule/MTL/month/now`);
+    const response = await fetch(`/api/club-schedule/MTL/month/now`);
     return response.json();
 }
 
 // Récuperer 3-4 dernière games et 7-8 prochaines
 export async function getScoreboard() {
-    const response = await fetch(`${BASE_URL}/scoreboard/mtl/now`);
+    const response = await fetch(`/api/scoreboard/mtl/now`);
     return response.json();
 }
 
 // Récuperer liste des joueurs et leur stats selon saison et type
 export async function getStats(season, gameType) {
-    const response = await fetch(`${BASE_URL}/club-stats/MTL/${season}/${gameType}`);
+    const response = await fetch(`/api/club-stats/MTL/${season}/${gameType}`);
     return response.json();
 }
 
 // Récuperer liste des saisons disponibles
 export async function getSeasons() {
-    const response = await fetch(`${BASE_URL}/club-stats-season/MTL`);
+    const response = await fetch(`/api/club-stats-season/MTL`);
     return response.json();
 }
 
 // Récuperer localisation (pays) du user
 export async function getLocation() {
-    const response = await fetch(`${BASE_URL}/location`);
+    const response = await fetch(`/api/location`);
     return response.json();
 }
 
 // Récuperer stats d'un game en particulier
 export async function getGameInfo(gameId) {
-    const response = await fetch(`${BASE_URL}/gamecenter/${gameId}/boxscore`);
+    const response = await fetch(`/api/gamecenter/${gameId}/boxscore`);
     return response.json();
 }
 
--- src/worker.js
+++ src/worker.js
@@ -1,16 +1,7 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-import manifestJSON from "__STATIC_CONTENT_MANIFEST";
-const assetManifest = JSON.parse(manifestJSON);
-
 export default {
-  async fetch(request, env, ctx) {
-    try {
-      return await getAssetFromKV(
-        { request, waitUntil: ctx.waitUntil.bind(ctx) },
-        { ASSET_NAMESPACE: env.__STATIC_CONTENT, ASSET_MANIFEST: assetManifest }
-      );
-    } catch (e) {
-      return new Response("Not found", { status: 404 });
-    }
-  }
+    async fetch(request, env, ctx) {
+        // Static assets are served automatically by Cloudflare
+        // Add any custom Worker logic here if needed
+        return env.ASSETS.fetch(request);
+    },
 };
